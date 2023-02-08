26836e6039ec26ddcb496ee2c1a8a0392e421a68
Fullstak-nl <bram.h@fullstak.nl>
2023-02-09 00:24:49 +0100

    Fixed loading JSON and font files. And enhanced layout creation

--- src/generators/pdfGenerator.js
+++ src/generators/pdfGenerator.js
@@ -1,24 +1,28 @@
 import PDFDocument from 'pdfkit'
 import getStream from "get-stream"
 import fs from 'fs'
+
 class PdfGenerator {
     constructor() {
-        this.PDFDocument = PDFDocument;
-        this.getStream = getStream;
-        this.top = 0;
-        this.item = 0;
+        this.PDFDocument = PDFDocument
+        this.getStream = getStream
+        this.top = 0
+        this.item = 0
+        this.lastHeight = 0
+        this.margin = { top: 0, left: 0, right: 0, bottom: 0 }
     }
 
     async startPdf(options) {
         const doc = new this.PDFDocument({
             size: options?.pdf?.settings?.format,
             margin: options?.pdf?.settings?.margin,
-        });
+        })
+        this.margin = options?.pdf?.settings?.margin
+        this.top = this.margin.top
         if (options?.pdf?.settings?.font && typeof options?.pdf?.settings?.font !== 'string') {
             try {
-                const font = await fetch(`./../../src/fonts/${options?.pdf?.settings?.font?.file}`)
-                const arrayBuffer = await font.arrayBuffer()
-                doc.registerFont(options?.pdf?.settings?.font?.name, arrayBuffer)
+                const fontBuffer = fs.readFileSync(`${process.cwd()}/src/fonts/${options?.pdf?.settings?.font?.file}`)
+                doc.registerFont(options?.pdf?.settings?.font?.name, fontBuffer)
             }catch (e) {
                 console.log("Font error: ",e)
             }
@@ -32,8 +36,8 @@ class PdfGenerator {
             try {
                 let layout = []
                 if(header?.content) {
-                    const layoutJSON = await fetch(`./../../src/layouts/${header?.content}`)
-                    layout = await layoutJSON.json()
+                    const layoutJSON = fs.readFileSync(`${process.cwd()}/src/layouts/${header.content}`)
+                    layout = JSON.parse(layoutJSON)
                 }
                 layout.forEach((layoutItem) => this.generateElement(doc,layoutItem))
                 this.top += header?.height??50
@@ -50,8 +54,8 @@ class PdfGenerator {
             try {
                 let layout = []
                 if(footer?.content) {
-                    const layoutJSON = await fetch(`./../../src/layouts/${footer?.content}`)
-                    layout = await layoutJSON.json()
+                    const layoutJSON = fs.readFileSync(`${process.cwd()}/src/layouts/${footer.content}`)
+                    layout = JSON.parse(layoutJSON)
                 }
                 layout.forEach((layoutItem) => this.generateElement(doc,layoutItem))
                 this.top += footer?.height??50
@@ -63,7 +67,7 @@ class PdfGenerator {
     }
 
     parseVariables(text, data) {
-        const regex = /{{\s([^{]+)\s}}/gi
+        const regex = /{{\s(.*?)(?=\s}})\s}}/ig
         return text.replace(regex, (match, key)=>{
             let [keys, filter] = key.split(' | ')
             keys = keys.split('.')
@@ -74,10 +78,12 @@ class PdfGenerator {
                 if(filter.startsWith('date')) {
                     const dateRegex = /date\(['"]([^'"]+)['"][\,\s]{0,2}([^))]*)\)/i
                     const [_, locale, format] = dateRegex.exec(filter)
-                    value = new Date(value).toLocaleDateString(locale, JSON.parse(format.replace("'",'"'))??{})
+                    value = new Date(value).toLocaleDateString(locale, JSON.parse(format.replaceAll("'",'"'))??{})
                 }else if(filter.startsWith('currency')) {
                     const numberRegex = /currency\(['"]([^'"]+)['"]\)/i
                     const [_, locale] = numberRegex.exec(filter)
+                    if(typeof value === 'string')
+                        value = parseFloat(value.replace(data?.currency_code.toUpperCase(),''))
                     value = new Intl.NumberFormat(locale, {style:'currency',currency: data?.currency_code.toUpperCase()}).format(value/100)
                 }
             }
@@ -93,36 +99,42 @@ class PdfGenerator {
                     imageOptions.align = layoutItem?.align
                 if(layoutItem?.valign)
                     imageOptions.valign = layoutItem?.valign
-                doc.image(`./../../src/images/${layoutItem.image}`, layoutItem.x, layoutItem.y, imageOptions)
+                doc.image(`${process.cwd()}/src/images/${layoutItem.image}`, layoutItem.x, this.margin.left+layoutItem.y, imageOptions)
                 break;
             case 'text':
-                const textOptions = {}
-                for(const key in layoutItem) {
-                    if(['type','color','font','size','text', 'x', 'y'].includes(key))
-                        continue
-                    textOptions[key] = layoutItem[key]
-                }
                 if(layoutItem?.color)
                     doc.fillColor(layoutItem?.color)
                 if(layoutItem?.font)
                     doc.font(layoutItem?.font)
                 if(layoutItem?.size)
                     doc.fontSize(layoutItem?.size)
-                if(layoutItem?.x&&layoutItem?.y)
-                    doc.text(this.parseVariables(layoutItem?.text, data), layoutItem?.x, layoutItem?.y, Object.fromEntries(Object.entries(layoutItem).filter(([key]) => !['type','color','font','size','text', 'x', 'y'].includes(key))))
+                if(layoutItem?.width && typeof layoutItem?.width === 'string')
+                    layoutItem.width = parseInt(doc.page.width - this.margin.left - this.margin.right)
+                else if(layoutItem?.width && typeof layoutItem?.width === 'number')
+                    layoutItem.width = parseInt(layoutItem?.width)
+                const parsedText = this.parseVariables(layoutItem?.text, data)
+                const textOptions = Object.fromEntries(Object.entries(layoutItem).filter(([key]) => !['type','color','font','size','text', 'x', 'y'].includes(key)))
+                this.lastHeight = doc.heightOfString(parsedText,textOptions)
+                if(layoutItem?.x||layoutItem?.y)
+                    doc.text(this.parseVariables(layoutItem?.text, data), this.margin.left + (layoutItem?.x??0), layoutItem?.y??this.top, textOptions)
                 else
-                    doc.text(this.parseVariables(layoutItem?.text, data), Object.fromEntries(Object.entries(layoutItem).filter(([key]) => !['type','color','font','size','text', 'x', 'y'].includes(key))))
+                    doc.text(this.parseVariables(layoutItem?.text, data), this.margin.left, this.top, textOptions)
+                //this.top += textHeight
                 break;
             case 'moveDown':
-                doc.moveDown(layoutItem?.lines??1)
-                this.top += doc._fontSize * (layoutItem?.lines??1)
+                //doc.moveDown(layoutItem?.lines??1)
+                // fake movedown by altering this.top + last fontsize * 1.5 or use measured height
+                if(layoutItem?.useMeasure)
+                    this.top += parseInt(this.lastHeight)
+                else
+                    this.top += (parseInt(doc._fontSize) * 1.5) * (layoutItem?.lines??1)
                 break;
             case 'hr':
                 doc
                     .strokeColor(layoutItem?.color??'#aaaaaa')
                     .lineWidth(layoutItem?.width??1)
-                    .moveTo(0, layoutItem?.y??this.top)
-                    .lineTo(layoutItem?.width??doc.page.width, layoutItem?.y??this.top)
+                    .moveTo(layoutItem?.x??this.margin.left, layoutItem?.y??this.top)
+                    .lineTo(layoutItem?.width??(doc.page.width-this.margin.right), layoutItem?.y??this.top)
                     .stroke()
                 this.top += layoutItem?.height??10
                 break;
@@ -133,6 +145,7 @@ class PdfGenerator {
                     doc.font(layoutItem?.font)
                 if(layoutItem?.size)
                     doc.fontSize(layoutItem?.size)
+                let xPos = this.margin.left+(layoutItem?.x??0)
                 layoutItem?.columns?.forEach((column) => {
                     if(column?.color)
                         doc.fillColor(column?.color)
@@ -140,8 +153,19 @@ class PdfGenerator {
                         doc.font(column?.font)
                     if(column?.size)
                         doc.fontSize(column?.size)
-                    doc.text(this.parseVariables(column?.text, data), column?.x, column?.y??this.top, Object.fromEntries(Object.entries(column).filter(([key]) => !['type','color','font','size','text', 'x', 'y'].includes(key))))
+                    if(column?.width && typeof column?.width === 'string')
+                        column.width = parseInt(doc.page.width - this.margin.left - this.margin.right)
+                    else if(column?.width && typeof column?.width === 'number')
+                        column.width = parseInt(column?.width)
+                    const parsedText = this.parseVariables(column?.text, data)
+                    const columnOptions = Object.fromEntries(Object.entries(column).filter(([key]) => !['type','color','font','size','text', 'x', 'y'].includes(key)))
+                    doc.text(parsedText, xPos, column?.y??this.top, columnOptions)
+                    if(column?.width)
+                        xPos += column?.width
+                    else
+                        xPos += doc.widthOfString(parsedText, columnOptions)
                 })
+                this.top += doc._fontSize * 1.5
                 break;
             default:
                 break;
@@ -153,8 +177,8 @@ class PdfGenerator {
         const doc = await this.startPdf(options)
         await this.generateHeader(doc, options)
         try{
-            const layoutJSON = await fetch(`./../../src/layouts/${options?.templates?.invoice}`)
-            const layout = await layoutJSON.json()
+            const layoutJSON = fs.readFileSync(`${process.cwd()}/src/layouts/${options?.pdf?.templates?.invoice}`)
+            const layout = JSON.parse(layoutJSON)
             let itemLayout = []
             let itemLayoutRunning = false;
             Object.values(layout).forEach((layoutItem) => {
@@ -165,10 +189,10 @@ class PdfGenerator {
                     itemLayoutRunning = false
                     order.items.forEach((item, index) => {
                         this.item = index
-                        itemLayout.forEach((layoutItem) => this.generateElement(doc,layoutItem, data))
+                        itemLayout.forEach((layoutItem) => this.generateElement(doc,layoutItem, order))
                     })
                 }else
-                    this.generateElement(doc, layoutItem, {order})
+                    this.generateElement(doc, layoutItem, order)
             })
         }catch (e) {
             console.log("Invoice error: ",e)
